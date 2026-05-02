8dfb230682bc38cafd9de68141c7a89c12e61a69
Matthias-3-4-5CHEL <makreuzbichler@tsn.at>
2026-05-02 15:48:50 +0200

    Level Tree

---
+++ VINESWEEPER/level_tree.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_number_pressed() -> void:
+	pass # Replace with function body.
+	
+
+func _on_number2_pressed() -> void:
+	pass # Replace with function body.
+	
+
+func _on_number3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_number4_pressed() -> void:
+	pass # Replace with function body.
