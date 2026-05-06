91a32d91947c0e07e5b1c756c875a016f1bed0da
Matthias-3-4-5CHEL <makreuzbichler@tsn.at>
2026-05-06 21:15:02 +0200

    Buttons in Level Tree

--- VINESWEEPER/level_tree.gd
+++ VINESWEEPER/level_tree.gd
@@ -1,9 +1,20 @@
 extends Control
 
 
+var count = 3	# irgendein Wert welcher gezählt wurde
+@onready var level1 = $Stump/Level1
+@onready var level2 = $Stump2/Level2
+@onready var level3 = $Stump3/Level3
+@onready var level4 = $Stump4/Level4
+
+var levels
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	levels = [level1, level2, level3, level4]
+	for level in levels:
+		level.disabled = true
+	levels[count-1].disabled = false
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,10 +22,10 @@ func _process(delta: float) -> void:
 	pass
 
 
-
 func _on_number_pressed() -> void:
-	pass # Replace with function body.
+	pass
 	
+
 
 func _on_number2_pressed() -> void:
 	pass # Replace with function body.
