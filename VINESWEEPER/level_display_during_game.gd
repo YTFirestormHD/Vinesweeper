09d58cb6dc74a967acd624959b7f28a0dee0ae53
YTFirestormHD <moritzvongruber@gmail.com>
2026-05-22 23:27:28 +0200

    removed level display again

--- VINESWEEPER/level_display_during_game.gd
+++ VINESWEEPER/level_display_during_game.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	set_text("Level: " + str(GLOBAL.current_level))
+	#set_text("Level: " + str(GLOBAL.current_level))
+	pass
