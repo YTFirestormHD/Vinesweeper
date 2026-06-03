e66ff162e9f28ee4572f57c9d0f4ee6b96ff2134
Matthias-3-4-5CHEL <makreuzbichler@tsn.at>
2026-06-03 22:35:48 +0200

    Added more Events and making them random

--- VINESWEEPER/events.gd
+++ VINESWEEPER/events.gd
@@ -1,10 +1,21 @@
 extends Control
-@onready var stat_increase: Label = $TextBox/Event_StatIncrease
 
-# Called when the node enters the scene tree for the first time.
+@onready var statincrease: Label = $TextBox/Event_StatIncrease
+@onready var coins: Label = $TextBox/Event_Coins
+@onready var first_aid: Label = $TextBox/Event_FirstAid
+@onready var discount: Label = $TextBox/Event_Discount
+
+var events = []
+
 func _ready() -> void:
-	pass # Replace with function body.
+	randomize()
+	events = [statincrease, coins, first_aid, discount]
 
+	for event in events:
+		event.visible = false
+
+	var zufaelliges_event = events[randi() % events.size()]
+	zufaelliges_event.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
